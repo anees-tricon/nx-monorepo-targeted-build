5cc734d50a2099dfa9289a9dbc38db30148fe5b9
Anees Mohamed <anees@triconinfotech.com>
2022-06-12 20:49:48 +0530

    feat: initial commit of 3 apps

---
+++ apps/search/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'targeted-build-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'search';
+}
